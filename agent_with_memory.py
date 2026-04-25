9d4a259c5c40dfc44d58cdd6d9b27315f9b6c58d
KavindhyaJay <kavindhyauj@gmail.com>
2026-04-25 22:09:21 +0530

    agent with memory

--- agent_with_memory.py
+++ agent_with_memory.py
@@ -8,7 +8,9 @@ from memory_manager import save_to_memory, get_recent_memory
 load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
 
-model = genai.GenerativeModel("gemini-2.5-flash", api_key=API_KEY)
+client = genai.Client(api_key=API_KEY)
+
+MODEL = "gemini-2.5-flash"
 
 def ai_agent_with_memory(user_input):
     #load last 3 interactions
@@ -22,20 +24,27 @@ def ai_agent_with_memory(user_input):
     final_prompt = f"""
 You are an AI assistant with memory. 
 Here is your past memory:
-    
-   
+
 {memory_text}
 
 Now answer the new user message:
 
 {user_input}
-    """
+"""
+    try:
+        response = client.models.generate_content(
+            model=MODEL,
+            contents=final_prompt
+        )
+        
+        answer = response.text
 
-    # Generate the response
-    response = model.generate_content(final_prompt)
-    answer = response.text
+        # Save both user query + AI answer
+        save_to_memory(user_input, answer)
 
-    # Save both user query + AI answer
-    save_to_memory(user_input, answer)
+        return answer
+    except Exception as e:
+        return f"Error: {str(e)}"
 
-    return answer
+   
+    
