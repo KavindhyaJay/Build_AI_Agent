1ca55bd0e57ee643e4a729bb18858e7f84c0de1a
KavindhyaJay <kavindhyauj@gmail.com>
2026-04-25 19:13:34 +0530

    test memory agent

---
+++ test_memory_agent.py
@@ -0,0 +1,10 @@
+from agent_with_memory import ai_agent_with_memory
+
+print("AI Agent with Memory(type 'exit' to quit)\n) - test_memory_agent.py:3")
+
+while True:
+    user_msg = input("You: ")
+    if user_msg.lower() == "exit":
+        break
+    reply = ai_agent_with_memory(user_msg)
+    print(f"Agent: {reply} - test_memory_agent.py:10")
