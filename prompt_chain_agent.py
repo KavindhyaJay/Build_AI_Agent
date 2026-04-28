f1398c21b444528bd94add088416f30fb0ff0704
KavindhyaJay <kavindhyauj@gmail.com>
2026-04-28 19:58:04 +0530

    Prompt chain agent

--- prompt_chain_agent.py
+++ prompt_chain_agent.py
@@ -6,27 +6,33 @@ from memory_manager import save_to_memory, get_recent_memory
 load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
 
-model = genai.GenerativeModel("gemini-2.5-flash", api_key=API_KEY)
+client = genai.Client(api_key=API_KEY)
+
+MODEL = "gemini-2.5-flash"
 
 def chain(prompt):
 
     #step 1: ask agent to brake doen the task
-    step_1 = model.generate_content(
-        f"Break this task into clear steps: \n{prompt}"
+    step_1 = client.models.generate_content(
+        model=MODEL,
+        contents=f"Break this task into clear steps: \n{prompt}"
     ).text
-    print("\nStep 1  Breakdown:\n  agent_with_memory.py:17 - prompt_chain_agent.py:17", step_1)
+        
+    print("\nStep 1  Breakdown:\n  agent_with_memory.py:17 - prompt_chain_agent.py:21", step_1)
     
     #step 2: ask agent to complete the reasoning
-    step_2 = model.generate_content(
-        f"Based on the steps, give the detailed explanation: \nSteps: {step_1}\nTask: {prompt}"
+    step_2 = client.models.generate_content(
+        model=MODEL,
+        contents=f"Based on the steps, give the detailed explanation: \nSteps: {step_1}\nTask: {prompt}"
     ).text
-    print("\n[Explanation]  agent_with_memory.py:23 - prompt_chain_agent.py:23", step_2)
+    print("\n[Explanation]  agent_with_memory.py:23 - prompt_chain_agent.py:28", step_2)
 
     #step 3 Ask agent for final summary
-    step_3 = model.generate_content(
-        f"Summarize the final answer in simple words: \nExplanation: {step_2}\nTask: {propmt}"
+    step_3 = client.models.generate_content(
+        model=MODEL,
+        contents=f"Summarize the final answer in simple words: \nExplanation: {step_2}\nTask: {prompt}"
     ).text
-    print("\n[Summary]  agent_with_memory.py:29 - prompt_chain_agent.py:29", step_3)
+    print("\n[Summary]  agent_with_memory.py:29 - prompt_chain_agent.py:35", step_3)
 
     return step_3
 
