de298ed9daf5a06459d5eb1654c80c90370eff9a
KavindhyaJay <kavindhyauj@gmail.com>
2026-04-28 19:58:34 +0530

    Router Agent

---
+++ router_agent.py
@@ -0,0 +1,31 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+from api_tools import get_current_time
+
+load_dotenv()
+API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=API_KEY)
+
+MODEL = "gemini-2.5-flash"
+
+def router_agent(query):
+    
+    # Simple routing logic
+    if "time" in query.lower():
+        return f"The current time is: {get_current_time()}"
+    if "explain" in query.lower():
+        explanation = client.models.generate_content(
+            model=MODEL,
+            contents=f"Answer the following question: {query}"
+        ).text
+        return explanation
+    
+    # Default fallback
+    response = client.models.generate_content(query).text
+    return response
+
+if __name__ == "__main__":
+    print(router_agent("What is the time now? - router_agent.py:30"))
+    print(router_agent("Explain recursion - router_agent.py:31"))
